3b698352497fe32f680c8f80b1feba1b6a8e16b0
sales <mariaclaradeasales@gmail.com>
2024-09-27 21:47:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿decimal valorcompra, valorpago, valortroco;
+
+Console.WriteLine("--- troco ---\n");
+
+Console.Write("Digite o valor da compra:");
+valorcompra = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite o valor pago:");
+valorpago = Convert.ToDecimal(Console.ReadLine());
+
+valortroco = valorpago - valorcompra;
+
+Console.WriteLine($"\nTroco: {valortroco}:c");
+
+
